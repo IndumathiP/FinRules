5290fa59f66cbd45916dd97f601376c71d1c30db
Vinitha1595 <vinitha.c@tavant.com>
2018-03-21 18:14:06 +0530

    Pipeline page is complete

--- FinRulesCodeBase/assets/js/pipelinescript.js
+++ FinRulesCodeBase/assets/js/pipelinescript.js
@@ -1,19 +1,54 @@
-var counter = { one:0, two:0};
+var counter = 0;
+// document.getElementById("list").addEventListener("click",addIcon);
+function addIcon(event) {
+  var counter;
+  counter=window.event.currentTarget.getAttribute("counter");
 
-function addIcon(e) {
-  for(var i in counter){
-    
+  
+  if(counter=="0")
+  {
+      window.event.currentTarget.children[0].style.transform="rotate(180deg)";
+      window.event.currentTarget.setAttribute("counter","1");
+      window.event.currentTarget.parentNode.parentNode.style.border = "1px solid #E7E7E7";
   }
-  counter += 1;
-  positionIcon(counter);
+  else {
+      window.event.currentTarget.children[0].style.transform="rotate(0deg)";
+      window.event.currentTarget.setAttribute("counter","0");
+      window.event.currentTarget.parentNode.parentNode.style.border = "0";
+  }
 }
 
-// function positionIcon(counter) {
-//   if (counter % 2 == 0) {
-//     document.getElementById("rotateArrow").style.transform = "rotate(0deg)";
-//     document.getElementsByClassName("recordInformation")[0].style.border = "0";
-//   } else {
-//     document.getElementById("rotateArrow").style.transform = "rotate(180deg)";
-//     document.getElementsByClassName("recordInformation")[0].style.border = "1px solid #E7E7E7";
-//   }
-// }
+document.getElementById("showFilters").addEventListener("click", showFilters);
+
+function display(ele, res) {
+  if (res) {
+    ele.classList.remove("displayNone");
+  } else {
+    ele.classList.add("displayNone");
+  }
+}
+
+function showFilters() {
+  document.getElementById("filter").classList.add("showFilters");
+  showOverlay();
+}
+function hideFilters() {
+  document.getElementById("filter").classList.remove("showFilters");
+  hideOverlay();
+}
+function showOverlay() {
+  document.getElementById("duFindingsWrapper").classList.add("duFindingsWrapper");
+}
+
+function hideOverlay() {
+  document.getElementById("duFindingsWrapper").classList.remove("duFindingsWrapper");
+}
+
+window.addEventListener("click",hideMenu);
+
+function hideMenu(e) {
+  console.log(e.target);
+  if(e.target ==  document.getElementById("duFindingsWrapper")){
+    hideFilters();    
+  }
+}
