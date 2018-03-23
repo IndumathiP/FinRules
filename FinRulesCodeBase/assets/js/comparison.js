091729984eff07fbd782e305ef1c9cb00df2d9d0
Vinitha1595 <vinitha.c@tavant.com>
2018-03-23 14:54:42 +0530

    Du-Lp comparison is complete

---
+++ FinRulesCodeBase/assets/js/comparison.js
@@ -0,0 +1,23 @@
+var n,data;
+function displayDuLp(m){
+ n=m;
+ sessionStorage.setItem('key',n);
+}
+(function () {
+  data = sessionStorage.getItem('key');
+  //sessionStorage.removeItem('key');
+  //sessionStorage.clear();
+  if (data==1)
+  {
+    document.getElementsByClassName("verification")[0].classList.remove("none");
+    document.getElementsByClassName("navigationWrapper")[0].classList.remove("none");
+    document.getElementsByClassName("lpFindings")[0].classList.add("none"); 
+  }
+  else if (data==2) {
+    document.getElementsByClassName("lpFindings")[0].classList.remove("none"); 
+    document.getElementsByClassName("verification")[0].classList.add("none");
+    document.getElementsByClassName("navigationWrapper")[0].classList.add("none");
+  }
+  
+  
+})();
