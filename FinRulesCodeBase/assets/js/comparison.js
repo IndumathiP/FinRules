6433383836372d4bd50494586ae1aed250e14c34
VimalPrasaath <vimal.viswanathan@tavant.com>
2018-03-26 12:12:36 +0530

    modified

--- FinRulesCodeBase/assets/js/comparison.js
+++ FinRulesCodeBase/assets/js/comparison.js
@@ -1,37 +1,29 @@
-var n,data,counter=1;
-function displayDuLp(m){
- n=m;
- sessionStorage.setItem('key',n);
+var n, data, counter = 1;
+function displayDuLp(m) {
+  n = m;
+  sessionStorage.setItem('key', n);
 }
 (function () {
   data = sessionStorage.getItem('key');
-  //sessionStorage.removeItem('key');
-  //sessionStorage.clear();
-  if (data==1)
-  {
+  if (data == 1) {
     document.getElementsByClassName("verification")[0].classList.remove("none");
     document.getElementsByClassName("navigationWrapper")[0].classList.remove("none");
-    document.getElementsByClassName("lpFindings")[0].classList.add("none"); 
-  //  document.getElementsByClassName("footer")[0].classList.remove("bottom");
+    document.getElementsByClassName("lpFindings")[0].classList.add("none");
   }
-  else if (data==2) {
-    document.getElementsByClassName("lpFindings")[0].classList.remove("none"); 
+  else if (data == 2) {
+    document.getElementsByClassName("lpFindings")[0].classList.remove("none");
     document.getElementsByClassName("verification")[0].classList.add("none");
     document.getElementsByClassName("navigationWrapper")[0].classList.add("none");
-  //  document.getElementsByClassName("footer")[0].classList.add("bottom");
   }
-  
-  
 })();
 function rotate() {
-  if(counter&1)
-  {
-    document.getElementsByClassName("downIcon")[0].style.transform="rotate(180deg)";
-    counter=counter+1;
+  if (counter & 1) {
+    document.getElementsByClassName("downIcon")[0].style.transform = "rotate(180deg)";
+    counter = counter + 1;
   }
-  else{
-    document.getElementsByClassName("downIcon")[0].style.transform="rotate(0deg)";
-    counter=counter+1;
+  else {
+    document.getElementsByClassName("downIcon")[0].style.transform = "rotate(0deg)";
+    counter = counter + 1;
   }
-  
+
 }
