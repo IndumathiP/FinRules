418a929059dee5b98d5c3ceb1c98f596bc40592e
Madhu Bala V <madhu.bala@tavant.com>
2018-03-26 16:48:19 +0530

    UI changes

--- FinRulesCodeBase/assets/js/comparison.js
+++ FinRulesCodeBase/assets/js/comparison.js
@@ -16,14 +16,23 @@ function displayDuLp(m) {
     document.getElementsByClassName("navigationWrapper")[0].classList.add("none");
   }
 })();
-function rotate() {
+function rotate(event) {
   if (counter & 1) {
     document.getElementsByClassName("downIcon")[0].style.transform = "rotate(180deg)";
+    window.event.currentTarget.style.top = "0";
+    window.event.currentTarget.style.bottom = "";
+    window.event.currentTarget.style.transition = "top 1s ease-in";
+    document.getElementsByClassName("reasonForNotEligible")[0].style.display = "block";
+    window.event.currentTarget.previousElementSibling.style.opacity = "0";
     counter = counter + 1;
   }
   else {
     document.getElementsByClassName("downIcon")[0].style.transform = "rotate(0deg)";
     counter = counter + 1;
+    window.event.currentTarget.style.bottom = "0";
+    window.event.currentTarget.style.top = "";
+    document.getElementsByClassName("reasonForNotEligible")[0].style.display = "none";
+    window.event.currentTarget.previousElementSibling.style.opacity = "1";
   }
 
 }
