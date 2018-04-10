538f139121a086da2b07d5e1779677d8c0ced2b0
Vinitha1595 <vinitha.c@tavant.com>
2018-04-10 18:53:04 +0530

    Add Condition Flow is complete

--- FinRulesCodeBase/assets/js/conditionFlowScript.js
+++ FinRulesCodeBase/assets/js/conditionFlowScript.js
@@ -1,3 +1,4 @@
+var count = 0;
 function rotate(event) {
   //window.event.currentTarget.children[0].classList.add("rotate");
   if(count&1)
@@ -9,6 +10,13 @@ function rotate(event) {
       window.event.currentTarget.children[0].style.transform="rotate(180deg)";
   }
   count=count+1;
+}
+
+function showcondition(){
+  if(document.getElementById("category").value == "Tip Income"){
+    document.getElementById("selectCategory").classList.add("showflow");
+    document.getElementById("showtask").classList.add("showflow");
+  }
 }
 
 (function () {
