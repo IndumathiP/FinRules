a0f83beeaf990cec133c5a471aabfc9c965cfbad
Vinitha1595 <vinitha.c@tavant.com>
2018-04-13 18:32:47 +0530

    Dropdown added for conditionflow

--- FinRulesCodeBase/assets/js/conditionFlowScript.js
+++ FinRulesCodeBase/assets/js/conditionFlowScript.js
@@ -19,6 +19,67 @@ function showcondition(){
   }
 }
 
+
+// select multiple checkbox
+
+function show() {
+  if(flag) {
+    document.getElementById('options').classList.add('inline-block');
+  }
+  else {
+    document.getElementById('options').classList.remove('inline-block');
+  }
+  flag = !flag;
+}
+function display(e) {
+  if(this.checked) {
+    selected = [];
+    checkboxes.forEach(checkbox => {
+      if(checkbox.checked){
+        selected.push(checkbox.name);
+      }
+    })
+    // selected.push(e.target.name); 
+    selectedValues.value = selected.join(', ');
+    // document.getElementById('count').innerHTML = "(" + selected.length + ")";
+  }
+  else {
+    selected = selected.filter(element => element!=e.target.name);
+    selectedValues.value = selected.join(', ');
+    // document.getElementById('count').innerHTML = "(" + selected.length + ")";
+  }
+}
+function toSelectAll(e) {
+  if(this.checked) {
+    selected = [];
+    checkboxes.forEach(checkbox => { 
+      if(!checkbox.disabled) {
+        checkbox.checked = true; selected.push(checkbox.name);
+      }
+    }); 
+    selectedValues.value = "All Browsers";
+    // document.getElementById('count').innerHTML = "(" + selected.length + ")";
+  }
+  else {
+    checkboxes.forEach(checkbox => checkbox.checked = false);
+    selected = [];
+    selectedValues.value = "";
+    // document.getElementById('count').innerHTML = "(" + selected.length + ")";
+  }
+}
+
+//get elements
+var flag = true;
+var selected = [];
+var dropdown = document.getElementById('dropdown');
+dropdown.addEventListener('click', show);
+var selectedValues = document.getElementById('selectedValues');
+var checkboxes = document.querySelectorAll('.carName');
+checkboxes.forEach(checkbox => checkbox.addEventListener('change',display));
+var selectAll = document.getElementById('selectAll');
+selectAll.addEventListener('change',toSelectAll);
+
+// active Menu
 (function () {
 var len,a,i;
 a=document.getElementsByClassName("mainMenu");
