4a74f03697ee432453f3f6174f1f9cba49484de0
Vinitha1595 <vinitha.c@tavant.com>
2018-04-16 16:24:37 +0530

    dropdown added

--- FinRulesCodeBase/assets/js/conditionFlowScript.js
+++ FinRulesCodeBase/assets/js/conditionFlowScript.js
@@ -22,15 +22,16 @@ function showcondition(){
 
 // select multiple checkbox
 
-function show() {
-  if(flag) {
-    document.getElementById('options').classList.add('inline-block');
-  }
-  else {
-    document.getElementById('options').classList.remove('inline-block');
-  }
-  flag = !flag;
-}
+// function show() {
+//   if(flag) {
+//     document.getElementById('options').classList.add('inline-block');
+//   }
+//   else {
+//     document.getElementById('options').classList.remove('inline-block');
+//   }
+//   flag = !flag;
+// }
+
 function display(e) {
   if(this.checked) {
     selected = [];
@@ -57,7 +58,7 @@ function toSelectAll(e) {
         checkbox.checked = true; selected.push(checkbox.name);
       }
     }); 
-    selectedValues.value = "All Browsers";
+    selectedValues.value = "All Borrowers";
     // document.getElementById('count').innerHTML = "(" + selected.length + ")";
   }
   else {
@@ -71,23 +72,43 @@ function toSelectAll(e) {
 //get elements
 var flag = true;
 var selected = [];
-// var dropdown = document.getElementById('dropdown');
 
-var selectedValues = document.getElementById('selectedValues');
-selectedValues.addEventListener('click', show);
+document.getElementById('selectedValues').addEventListener('click', myFunction);
+var selectedValues = document.getElementById('dropValue');
+
 var checkboxes = document.querySelectorAll('.carName');
 checkboxes.forEach(checkbox => checkbox.addEventListener('change',display));
 var selectAll = document.getElementById('selectAll');
 selectAll.addEventListener('change',toSelectAll);
 
+function myFunction() {
+  document.getElementById("options").classList.toggle("showDropdown");
+}
+
+
+window.onclick = function(event) {
+  if(typeof event != "function"){
+  if (!event.target.matches('.droptext')) {
+
+    var dropdowns = document.getElementsByClassName("options");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('showDropdown')) {
+        openDropdown.classList.remove('showDropdown');
+      }
+    }
+  }
+}
+};
+
 // active Menu
 (function () {
 var len,a,i;
-a=document.getElementsByClassName("mainMenu");
-len=a.length;
+a = document.getElementsByClassName("mainMenu");
+len = a.length;
 for(i=0;i<len;i++) {
   a[i].classList.remove("activeMenu");
-
 }
 document.getElementById("cond").classList.add("activeMenu");
 }) ();
